0479d2b2712b1dc6afbf6b52c52d6b593a2d78f9
zoynulabedin <trustcodernet@gmail.com>
2022-02-14 21:18:04 +0600

    update

---
+++ Froala.php
@@ -0,0 +1,256 @@
+<?php
+/*
+Plugin Name: Froala Elementor Addons
+Plugin URI:
+Description: Boilerplate for creating Elementor Extensions
+Version: 1.0
+Author: zoynul
+Author URI: https://wppixelfit.com
+License: GPLv2 or later
+Text Domain: froala-elementor-addons
+Domain Path: /languages/
+*/
+
+final class Froala_Elementor_Addons {
+
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * @var Elementor_Test_Extension The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @static
+	 *
+	 * @return Elementor_Test_Extension An instance of the class.
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function __construct() {
+
+		add_action( 'init', [ $this, 'i18n' ] );
+		add_action( 'plugins_loaded', [ $this, 'init' ] );
+
+	}
+
+	/**
+	 * Load Textdomain
+	 *
+	 * Load plugin localization files.
+	 *
+	 * Fired by `init` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function i18n() {
+
+		load_plugin_textdomain( 'froala-elementor-addons' );
+
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Load the plugin only after Elementor (and other plugins) are loaded.
+	 * Checks for basic plugin requirements, if one check fail don't continue,
+	 * if all check have passed load the files required to run the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init() {
+
+		// Check if Elementor installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+			return;
+		}
+
+		// Check for required Elementor version
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+			return;
+		}
+
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
+			return;
+		}
+
+		// Add Plugin actions
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
+        // froala categories
+        add_action( 'elementor/elements/categories_registered', [ $this, 'froala_elementor_category' ] );
+	}
+
+    /**
+     * froala elementor category
+     */
+
+    public function froala_elementor_category( $elements_manager ) {
+
+        $elements_manager->add_category(
+            'froala',
+            [
+                'title' => __( 'Froala', 'froala-elementor-addons' ),
+                'icon' => 'fa fa-image',
+            ]
+        );
+    }
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have Elementor installed or activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_missing_main_plugin() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'froala-elementor-addons' ),
+			'<strong>' . esc_html__( 'froala Elementor Addons', 'froala-elementor-addons' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'froala-elementor-addons' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'froala-elementor-addons' ),
+			'<strong>' . esc_html__( 'froala Elementor Addons', 'froala-elementor-addons' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'froala-elementor-addons' ) . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'froala-elementor-addons' ),
+			'<strong>' . esc_html__( 'froala Elementor Addons', 'froala-elementor-addons' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'froala-elementor-addons' ) . '</strong>',
+			self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Init Widgets
+	 *
+	 * Include widgets files and register them
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init_widgets() {
+
+		// Include Widget files
+		require_once( __DIR__ . '/widgets/Test.php' );
+
+		// Register widget
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Froala_Test_addons() );
+
+	}
+
+}
+
+Froala_Elementor_Addons::instance();
---
+++ widgets/Test.php
@@ -0,0 +1,217 @@
+<?php
+class Froala_Test_addons extends \Elementor\Widget_Base {
+
+/**
+ * Get widget name.
+ *
+ * Retrieve Blank widget name.
+ *
+ * @return string Widget name.
+ * @since 1.0.0
+ * @access public
+ *
+ */
+public function get_name() {
+    return 'Froala Test Widgets';
+}
+
+/**
+ * Get widget title.
+ *
+ * Retrieve Blank widget title.
+ *
+ * @return string Widget title.
+ * @since 1.0.0
+ * @access public
+ *
+ */
+public function get_title() {
+    return __( 'Froala Test Widgets', 'froala-elementor-addons' );
+}
+
+/**
+ * Get widget icon.
+ *
+ * Retrieve Blank widget icon.
+ *
+ * @return string Widget icon.
+ * @since 1.0.0
+ * @access public
+ *
+ */
+public function get_icon() {
+    return 'fa fa-code';
+}
+
+/**
+ * Get widget categories.
+ *
+ * Retrieve the list of categories the Blank widget belongs to.
+ *
+ * @return array Widget categories.
+ * @since 1.0.0
+ * @access public
+ *
+ */
+public function get_categories() {
+    return [ 'froala' ];
+}
+
+/**
+ * Register Blank widget controls.
+ *
+ * Adds different input fields to allow the user to change and customize the widget settings.
+ *
+ * @since 1.0.0
+ * @access protected
+ */
+protected function _register_controls() {
+
+    $this->register_content_controls();
+    $this->register_style_controls();
+
+}
+
+/**
+ * Register Blank widget content ontrols.
+ *
+ * Adds different input fields to allow the user to change and customize the widget settings.
+ *
+ * @since 1.0.0
+ * @access protected
+ */
+function register_content_controls() {
+    $this->start_controls_section(
+        'content_section',
+        [
+            'label' => __( 'Content', 'froala-elementor-addons' ),
+            'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+        ]
+    );
+
+    $this->add_control(
+        'dummy_text',
+        [
+            'label'       => __( 'Dummy Text', 'froala-elementor-addons' ),
+            'type'        => \Elementor\Controls_Manager::TEXTAREA,
+            'input_type'  => 'text',
+            'placeholder' => __( 'Some Dummy Text', 'froala-elementor-addons' ),
+            'default'     => __( 'Froala Test Widgets', 'froala-elementor-addons' ),
+        ]
+    );
+
+    $this->end_controls_section();
+}
+
+/**
+ * Register Blank widget style ontrols.
+ *
+ * Adds different input fields in the style tab to allow the user to change and customize the widget settings.
+ *
+ * @since 1.0.0
+ * @access protected
+ */
+protected function register_style_controls() {
+
+    $this->start_controls_section(
+        'style_section',
+        [
+            'label' => __( 'Text Style', 'froala-elementor-addons' ),
+            'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+        ]
+    );
+
+    $this->add_control(
+        'color',
+        [
+            'label'     => __( 'Color', 'froala-elementor-addons' ),
+            'type'      => \Elementor\Controls_Manager::COLOR,
+            'default'   => '#ff0000',
+            'selectors' => [
+                '{{WRAPPER}} .dummy_text' => 'color: {{color}}'
+            ]
+        ]
+    );
+
+    $this->add_control(
+        'alignment',
+        [
+            'type' => \Elementor\Controls_Manager::CHOOSE,
+            'label' => __( 'Alignment', 'froala-elementor-addons' ),
+            'default' => 'center',
+            'options' => [
+                'left' => [
+                    'title' => __( 'Left', 'froala-elementor-addons' ),
+                    'icon' => 'fa fa-align-left',
+                ],
+                'center' => [
+                    'title' => __( 'Center', 'froala-elementor-addons' ),
+                    'icon' => 'fa fa-align-center',
+                ],
+                'right' => [
+                    'title' => __( 'Right', 'froala-elementor-addons' ),
+                    'icon' => 'fa fa-align-right',
+                ],
+            ],
+            'selectors' => [
+                '{{WRAPPER}} .dummy_text' => 'text-align: {{alignment}}'
+            ]
+        ]
+    );
+
+
+    $this->add_group_control(
+        \Elementor\Group_Control_Typography::get_type(),
+        [
+            'name'     => 'content_typography',
+            'label'    => __( 'Typography', 'froala-elementor-addons' ),
+            'scheme'   => \Elementor\Scheme_Typography::TYPOGRAPHY_1,
+            'selector' => '{{WRAPPER}} .dummy_text',
+        ]
+    );
+
+    $this->end_controls_section();
+
+   
+}
+
+/**
+ * Render Blank widget output on the frontend.
+ *
+ * Written in PHP and used to generate the final HTML.
+ *
+ * @since 1.0.0
+ * @access protected
+ */
+protected function render() {
+
+    $settings   = $this->get_settings_for_display(); //and echo $settings['dummy_text']
+    $dummy_text = $this->get_settings( 'dummy_text' );
+    $color = $settings['color'];
+    $alignment = $settings['alignment'];
+    $this->add_render_attribute( 'dummy_text', 'class', 'dummy_text' );
+    $this->add_inline_editing_attributes( 'dummy_text' );
+    ?>
+    <div <?php echo $this->get_render_attribute_string( 'dummy_text' ) ?>> <?php echo esc_html( $dummy_text ); ?></div>
+    <?php
+
+
+}
+
+/**
+ * Render Blank widget output on the frontend.
+ *
+ * Written in JS and used to generate the final HTML.
+ *
+ * @since 1.0.0
+ * @access protected
+ */
+protected function _content_template() {
+    $this->add_render_attribute( 'dummy_text', 'class', 'dummy_text' );
+    $this->add_inline_editing_attributes( 'dummy_text', 'none' );
+    ?>
+    <div <?php echo $this->get_render_attribute_string( 'dummy_text' ) ?>> {{ settings.dummy_text }}</div>
+    <?php
+}
+
+}
